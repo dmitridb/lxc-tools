413dc8963beba84535f04aa105021acf5fc78aab
Dmitri DB <dmitridb@protonmail.com>
2020-07-22 10:19:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+#lxc-tools
+
+Dead simple scripts for creating and destroying LXC containers on 
+(so far only) debian-based distros
+
+adapted from https://github.com/mocig/server-tools
+updated to run on debian and popos
+written in perl and bash
+
+Ubuntu is leaning full in on pre-installed LXD, which is cool until it
+tries too hard to be like docker with a full-on bloat daemon running a REST API
+and all this extra stuff not needed in 90% of use cases for containerization.
+lxc-tools offers a dead-simple solution for firing up containers based on LXC 
+and LXC only, no frills or gimmicks necessary. These scripts also go through
+the steps necessary to remove systemd on the containers.
+
+This is still a work in progress...
+
+##TO-DO:
+Getting it to work on distros that aren't popos and debian, with out of
+the box support for non-debian/ubuntu containers as well
+
+The scripts in /extra need an update, they haven't been fully tested since
+this was originally written for debian stretch
+
+setup-lxc.sh
+```
+	Usage: ./setup-lxc.sh <lxc-network>
+	
+	Installs lxc and dnsmasq, configures bridge interface, network, nat
+	and places commands new-container and destroy-container in 
+    /usr/local/sbin
+
+	Asumes root, and a clean system (f.e. it will overwrite /etc/dnsmasq.conf)
+```
+
+new-container
+```
+    Usage: new-container <distro> <release> <containername>
+
+    Sets up a new container given the distro, release, and name of container
+```
+
+destroy-container
+```
+    destroy-container <containername>
+
+    Stops and removes a container.
+```
---
+++ setup-lxc.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -e
+if [ $# != 1 ]; then
+	echo "Usage: $0 lxc-network"
+	exit 1
+fi
+if [ `whoami` != "root" ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+source tools.sh
+if ! valid_ip $1; then 
+	echo "$1 is not a valid ip"
+	exit 1
+fi
+IFS='.' read -a NETWORK <<< "$1"
+if [ "${NETWORK[3]}" != "0" ]; then
+	echo "lxc-network needs to end with .0"
+	exit 1
+fi
+
+DEFIF=`ip route show |grep default|cut -d " " -f 5`
+echo "USING $DEFIF as outgoing"
+echo "Installing packages"
+apt-get update -qq
+apt-get install -y -qq net-tools lxc bridge-utils debootstrap iptables-persistent dnsmasq lxc-templates
+LXCNET="${NETWORK[0]}.${NETWORK[1]}.${NETWORK[2]}"
+echo "Adding bridge to interfaces"
+cat << EOF >> /etc/network/interfaces
+
+# nat bridge for lxc
+auto lxc-bridge-nat
+iface lxc-bridge-nat inet static
+        bridge_ports none
+        bridge_fd 0
+        bridge_maxwait 0
+        address ${NETWORK[0]}.${NETWORK[1]}.${NETWORK[2]}.1
+        netmask 255.255.255.0
+        up iptables -t nat -A POSTROUTING -o $DEFIF -s $1/24 -j MASQUERADE
+EOF
+/usr/sbin/ifup lxc-bridge-nat
+echo "Installing /usr/local/sbin/new-container"
+cat setup-lxc/new-container | sed "s/NETWORK/${NETWORK[0]}\.${NETWORK[1]}\.${NETWORK[2]}/" > /usr/local/sbin/new-container
+chmod 755 /usr/local/sbin/new-container
+echo "Installing /usr/local/sbin/destroy-container"
+cp setup-lxc/destroy-container /usr/local/sbin
+chmod 755 /usr/local/sbin/destroy-container
+#chmod 755 /usr/local/sbin/new-pihole
+echo "Enabling ip-forwarding"
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+/usr/sbin/sysctl -p
+echo "Use new-container command to setup a new debian-scretch container"
+if [ -f /etc/dnsmasq.conf ]; then
+  cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+fi
+cat << EOF > /etc/dnsmasq.conf
+interface=lxc-bridge-nat
+no-dhcp-interface=lxc-bridge-nat
+bind-interfaces
+EOF
+# TODO: Add determination whether host machine is your laptop on systemd or server on sysvinit
+# /etc/init.d/dnsmasq restart
+/usr/sbin/service dnsmasq restart
+echo "dnsmasq installed, script complete"
